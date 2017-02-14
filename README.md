06b813e5dd6c18cf1746924e8d819a00afe9b575
Jonathan Grim <jonjongrim@gmail.com>
2017-02-14 11:35:23 -0500

    Fix spaces of insertion_sort and add README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Algorithms in Python
+These Python modules implement various classic algorithms and were created as learning excersises for myself. Anyone is free to use or reference these as desired.
--- insertion_sort.py
+++ insertion_sort.py
@@ -1,25 +1,24 @@
-#! python
-
+#! /usr/bin/env python
+import random
 
 def insertion_sort(unsorted_list):
-	if len(unsorted_list) == 1:
-		return unsorted_list
+    if len(unsorted_list) == 1:
+        return unsorted_list
 
-	j = 1
-	while j < len(unsorted_list):
-		key = unsorted_list[j]
-		i = j - 1
+    j = 1
+    while j < len(unsorted_list):
+        key = unsorted_list[j]
+        i = j - 1
 
-		while i >= 0 and unsorted_list[i] > key:
-			unsorted_list[i+1] = unsorted_list[i]
-			i -= 1
+        while i >= 0 and unsorted_list[i] > key:
+            unsorted_list[i+1] = unsorted_list[i]
+            i -= 1
 
-		unsorted_list[i+1] = key
-		j += 1
+        unsorted_list[i+1] = key
+        j += 1
 
-	return unsorted_list
+    return unsorted_list
 
-
-print(insertion_sort([5, 2, 3, 1, 4]))
-print(insertion_sort([55, 50, 45, 40, 35, 20, 15, 10, 5, 3, 100]))
-print(insertion_sort([1, 2, 3, 4, 5]))
+rand_gen = (random.randint(1, 100) for x in range(20))
+unsorted_list = [x for x in rand_gen]
+print(insertion_sort(unsorted_list))
